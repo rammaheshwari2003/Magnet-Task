7fbf88af35566f0134fa069b33cf679e0ff681ba
Ram Maheshwari <130230374+rammaheshwari2003@users.noreply.github.com>
2025-05-26 20:03:09 +0530

    add to cart

---
+++ frontend/src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./css/style.css";
+import store from "./redux/store.jsx";
+import { Provider } from "react-redux";
+createRoot(document.getElementById("root")).render(
+<Provider store={store}>
+
+    <App />
+</Provider>
+    
+);
